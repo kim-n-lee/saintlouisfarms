844c2322888769582cb83e4d05b322485b3150ae
Mike Wilson <mikewilsonstl@gmail.com>
2023-07-03 17:15:10 -0500

    Initial commit

---
+++ src/main/java/org/liftoff/saintlouisfarms/SaintLouisFarmsApplication.java
@@ -0,0 +1,13 @@
+package org.liftoff.saintlouisfarms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaintLouisFarmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaintLouisFarmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/liftoff/saintlouisfarms/SaintLouisFarmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liftoff.saintlouisfarms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaintLouisFarmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
