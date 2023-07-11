baf8e48cd75b1fe822fa5bd5d9ae90b49d4e59e4
BlackRose1311 <mr.powers1311@gmail.com>
2023-07-11 12:38:39 -0500

    added farmer profile

--- src/main/java/org/liftoff/saintlouisfarms/data/UserRepository.java
+++ src/main/java/org/liftoff/saintlouisfarms/data/UserRepository.java
@@ -2,6 +2,7 @@ package org.liftoff.saintlouisfarms.data;
 
 
 import org.liftoff.saintlouisfarms.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByEmail(String email);
 
+    @Query(value = "SELECT * FROM farm.user where user.id= ?1", nativeQuery = true)
     User findById(int id);
 
 
