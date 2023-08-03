23776f874385ece8bbabee4df9db21d0f0e7b78a
Jzkili <125841589+Jzkili@users.noreply.github.com>
2023-08-03 04:09:54 -0500

    quantity limited

--- src/main/java/org/liftoff/saintlouisfarms/controllers/StoreController.java
+++ src/main/java/org/liftoff/saintlouisfarms/controllers/StoreController.java
@@ -154,7 +154,6 @@ public class StoreController {
                  shoppingBasketDTO.updateBasketItemQuantity(i, requestedQuantity);
             }
         }
-
 //        Sets quantity of cart to what is in DTO
 //        for(Integer i =0; i<shoppingBasketDTO.getBasketItemsAvailable().size();i++){
 //            currentShoppingBasket.getBasketItems().get(i).setQuantity(shoppingBasketDTO.getBasketItemsAvailable().get(i).getQuantity());
