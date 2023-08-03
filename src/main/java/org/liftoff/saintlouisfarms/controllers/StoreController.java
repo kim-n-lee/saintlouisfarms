f431ff78b8d9a595116cc352ad6848009b30cbf8
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-08-03 16:29:04 -0500

    The clientStore.html template was not loading up properly and I made some quick changes.

--- src/main/java/org/liftoff/saintlouisfarms/controllers/StoreController.java
+++ src/main/java/org/liftoff/saintlouisfarms/controllers/StoreController.java
@@ -119,7 +119,7 @@ public class StoreController {
         HttpSession session = request.getSession(false);
 
 //        Handling if user is not logged in, or is not client
-        if(session == null | !authenticationController.clientInSession(session))
+        if(session == null | authenticationController.clientInSession(session))
 //        ShoppingBasket should be saved until they login, after which client is set as the person that logged in
          {return "redirect:../login";}
 
@@ -146,8 +146,7 @@ public class StoreController {
 
              if (requestedQuantity > availableQuantityFromFarmer) {
                  String errorMessage = "This quantity is not available for " + currentBasketItem.getProduct().getName();
-                 redirectAttrs.addFlashAttribute("InsufficientQuantity", errorMessage);
-                 return "/store/clientStore";
+                 model.addAttribute("InsufficientQuantity", errorMessage);
             } else {
                 currentBasketItem.setQuantity(requestedQuantity);
                  currentShoppingBasket.getBasketItems().get(i).setQuantity(requestedQuantity);
