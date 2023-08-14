fff1340f9f8e8d5f1f3351f67f0a5eb41c4e3fc2
Mike Wilson <mikewilsonstl@gmail.com>
2023-08-13 19:18:55 -0500

    Added "View All Orders" to client navbar. Removed Profile option from dashboard and centered the fifth element. Removed buttons from dashboard and made the bootstrap cards clickable.

--- src/main/java/org/liftoff/saintlouisfarms/controllers/OrderController.java
+++ src/main/java/org/liftoff/saintlouisfarms/controllers/OrderController.java
@@ -186,7 +186,7 @@ public class OrderController {
 
         model.addAttribute("title", "Past Orders");
         model.addAttribute("orders", orders);
-        model.addAttribute("loggedIn", client != null);
+        model.addAttribute("clientLoggedIn", client != null);
         return "order/allOrders";
     }
 
